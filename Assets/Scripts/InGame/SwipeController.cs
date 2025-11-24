383b5e326bf3d29c52bb15a8373cb8bcc66c05e2
myeoungeun <melee0110@naver.com>
2025-11-24 17:18:39 +0900

    [feat] 클릭 시 범위 제한

--- Assets/Scripts/InGame/SwipeController.cs
+++ Assets/Scripts/InGame/SwipeController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SwipeController : MonoBehaviour
 {
     public Transform target;
+    public Camera camera;
 
     public bool isDrag = false;
     private Vector3 _lastMousePos;
@@ -13,13 +14,11 @@ public class SwipeController : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Touch");
             isDrag = true;
         }
         
         if (isDrag && Input.GetMouseButton(0))
         {
-            Debug.Log("Dragging");
             Vector3 delta = Input.mousePosition - _lastMousePos;
             target.Rotate(0f, delta.x * 0.1f, 0f, Space.World);
             _lastMousePos = Input.mousePosition;
@@ -28,7 +27,20 @@ public class SwipeController : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             isDrag = false;
-            Debug.Log("Touch End");
+            _lastMousePos = Input.mousePosition;
+            OnMouseDown();
         }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Mathf.Abs(camera.transform.position.z - target.position.z);
+        mousePosition = camera.ScreenToWorldPoint(mousePosition);
+        
+        if (mousePosition.x <= -4) mousePosition.x = -4;
+        else if (mousePosition.x >= 4) mousePosition.x = 4;
+        
+        Debug.Log(mousePosition);
     }
 }
